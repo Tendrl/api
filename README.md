33cdccca7d96107f45b3d1ae489e51381d564062
Anup <anivargi@redhat.com>
2016-10-02 23:22:43 +0200

    Create Gluster volume api

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# tendrl
+# Tendrl API
+### Installation
+```shell
+$ rbenv install 2.3.1 # Install ruby 2.3.1
+$ gem install bundler
+$ bundle install
+$ cp config/etcd.sample.tml to config/etcd.yml
+```
+### Start Server
+```shell
+  $ bundle exec shotgun -Ilib
+```
---
+++ spec/tendrl/component_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+RSpec.describe Tendrl::Component do
+
+  context "initialization" do
+
+    it 'sets the correct object type' do
+      expect(Tendrl::Component.new('gluster-3.8.3', 'volume').object_type).to eq('volume')
+    end
+
+    it 'raises an exception for undefined object type' do
+      expect{ Tendrl::Component.new('gluster-3.8.3', 'blah') }.to raise_error Tendrl::InvalidObjectError
+    end
+
+    it 'sets defined sds version' do
+      expect(Tendrl::Component.new('gluster-3.8.3', 'volume').sds_version).to eq('gluster-3.8.3')
+    end
+
+    it 'sets the defined attributes' do
+      expect(Tendrl::Component.new('gluster-3.8.3', 'volume').attributes).to include('volname', 'brickdetails')
+    end
+
+    it 'sets the defined performable actions' do
+      expect(Tendrl::Component.new('gluster-3.8.3', 'volume').actions).to include('info' => {"type"=>"get"})
+    end
+
+  end
+
+end
