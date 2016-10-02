66c5b529f3703de80b70c81e81fe71a1bfa9d0ae
Anup <anivargi@redhat.com>
2016-10-02 23:22:43 +0200

    Create Gluster volume api

---
+++ app.rb
@@ -0,0 +1,103 @@
+require 'bundler'
+require 'yaml'
+require 'sinatra/base'
+require "sinatra/multi_route"
+require 'securerandom'
+require 'active_support/core_ext/hash'
+require 'tendrl'
+
+Bundler.require :default, ENV['RACK_ENV'].to_sym
+
+class App < Sinatra::Base
+  register Sinatra::RespondWith
+  register Sinatra::CrossOrigin
+  register Sinatra::MultiRoute
+
+  set :root, File.dirname(__FILE__)
+
+  set :env, ENV['RACK_ENV'] || 'development'
+
+  enable :cross_origin
+
+  configure :development, :test do
+    set :etcd_config, Proc.new {
+      YAML.load_file('config/etcd.yml')[settings.env.to_sym] 
+    }
+  end
+
+  configure :production do
+    set :etcd_config, Proc.new {
+      YAML.load(ERB.new(File.read('config/etcd_prod.yml')).result)[settings.env.to_sym] 
+    }
+  end
+
+  before do
+    Tendrl.sds_config
+  end
+
+  set :etcd, Proc.new {
+    Etcd.client(
+      host: etcd_config[:host],
+      port: etcd_config[:port],
+      user_name: etcd_config[:user_name],
+      password: etcd_config[:password]
+    )
+  }
+
+  before do
+    @cluster = etcd.get("/clusters/#{params[:cluster_id]}")
+  end
+
+  get '/cluster/:cluster_id/:object_type/attributes' do
+    component = Tendrl::Component.new(@cluster[:sds_version],
+                                      params[:object_type])
+
+    respond_to do |f|
+      f.json { component.attributes.to_json }
+    end
+  end
+
+  get '/cluster/:cluster_id/:object_type/actions' do
+    component = Tendrl::Component.new(@cluster[:sds_version],
+                                      params[:object_type])
+
+    respond_to do |f|
+      f.json { component.actions.to_json }
+    end
+  end
+
+  post '/cluster/:cluster_id/:object_type/:action' do
+    component = Tendrl::Component.new(@cluster[:sds_version],
+                                      params[:object_type])
+    body = JSON.parse(request.body.read)
+    job_id = SecureRandom.hex
+    etcd.set("/queue/#{job_id}", value: {
+      cluster_id: params[:cluster_id],
+      sds_type: 'gluster',
+      action: params[:action],
+      object_type: params[:object_type],
+      status: 'processing',
+      attributes: body.slice(*component.attributes.keys)
+    })
+
+    job = { 
+      job_id: job_id,
+      status: 'processing',
+      sds_type: 'gluster',
+      action: params[:action],
+      object_type: params[:object_type] 
+    }
+
+    respond_to do |f|
+      status 202
+      f.json { job.to_json }
+    end
+  end
+
+  private
+
+  def etcd
+    settings.etcd
+  end
+
+end
---
+++ lib/tendrl/version.rb
@@ -0,0 +1,3 @@
+module Tendrl
+  VERSION = "0.0.1"
+end
