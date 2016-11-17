af64e40357d9fb5a1de1b07cff49ecf1b457f906
Anup <anivargi@redhat.com>
2016-11-18 02:08:45 +0530

    Removed unused gem dependencies

--- base.rb
+++ base.rb
@@ -1,7 +1,6 @@
 require 'bundler'
 require 'yaml'
 require 'sinatra/base'
-require "sinatra/multi_route"
 require 'securerandom'
 require 'active_support/core_ext/hash'
 require 'active_support/inflector'
@@ -10,21 +9,34 @@ require 'tendrl'
 Bundler.require :default, ENV['RACK_ENV'].to_sym
 
 class Base < Sinatra::Base
-  register Sinatra::RespondWith
-  register Sinatra::CrossOrigin
-  register Sinatra::MultiRoute
-
   set :root, File.dirname(__FILE__)
 
   set :env, ENV['RACK_ENV'] || 'development'
-
-  enable :cross_origin
 
   configure do
     set :etcd_config, Proc.new {
       YAML.load_file('config/etcd.yml')[settings.env.to_sym] 
     }
   end
+
+  set :http_allow_methods, [
+    'POST',
+    'GET',
+    'OPTIONS',
+    'PUT',
+    'DELETE'
+  ]
+
+  set :http_allow_headers, [
+    'Origin',
+    'Content-Type',
+    'Accept',
+    'Authorization',
+  ]
+
+  set :http_allow_origin, [
+    '*'
+  ]
 
   set :etcd, Proc.new {
     Etcd.client(
@@ -35,8 +47,20 @@ class Base < Sinatra::Base
     )
   }
 
+  before do
+    content_type :json
+    response.headers["Access-Control-Allow-Origin"] = 
+      settings.http_allow_origin.join(',')
+    response.headers["Access-Control-Allow-Methods"] = 
+      settings.http_allow_methods.join(',')
+    response.headers["Access-Control-Allow-Headers"] = 
+      settings.http_allow_headers.join(',')
+  end
+
   get '/ping' do
-    'pong'
+    { 
+      status: 'Ok'
+    }.to_json
   end
 
   get '/GetJobList' do
@@ -45,6 +69,18 @@ class Base < Sinatra::Base
 
   protected
 
+  # def http_allowed_methods
+  #   settings.http_allowed_methods.join(',')
+  # end
+  #
+  # def http_allowed_headers
+  #   settings.http_allowed_headers.join(',')
+  # end
+  #
+  # def http_allowed_origin
+  #   settings.http_allowed_origin.join(',')
+  # end
+  #
   def etcd
     settings.etcd
   end
