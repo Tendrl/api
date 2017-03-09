ea6b0be6ec574b81b8aaa701de3fd98e63989343
Mrugesh Karnik <mkarnik@redhat.com>
2017-03-09 17:29:00 +0530

    Initial CreateCluster implementation

---
+++ lib/tendrl/node.rb
@@ -0,0 +1,31 @@
+module Tendrl
+  class Node
+
+    # TODO: The interface sucks. Don't like making people use .nil? here and
+    # .exist? in the actual object.
+    def self.find_by_ip(ip)
+      begin
+        uuid = Tendrl.etcd.get("/indexes/ip/#{ip}").value
+      rescue Etcd::KeyNotFound
+        return nil
+      end
+      new(uuid)
+    end
+
+    attr_reader :uuid
+
+    def initialize(uuid)
+      @uuid   = uuid
+      @exists = true
+      begin
+        @path = Tendrl.etcd.get("/nodes/#{uuid}")
+      rescue Ectd::KeyNotFound
+        @exists = false
+      end
+    end
+
+    def exist?
+      @exists
+    end
+  end
+end
