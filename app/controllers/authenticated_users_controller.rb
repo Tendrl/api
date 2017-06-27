e31cc915bede75e83209fd5d5cb684322223bd7c
Anup <anivargi@redhat.com>
2017-06-27 13:29:23 +0530

    Feature expand cluster API

--- app/controllers/authenticated_users_controller.rb
+++ app/controllers/authenticated_users_controller.rb
@@ -24,6 +24,29 @@ class AuthenticatedUsersController < ApplicationController
 
   protected
 
+  def load_definitions(cluster_id)
+    definitions = etcd.get(
+      "/clusters/#{cluster_id}/_NS/definitions/data"
+    ).value
+    Tendrl.cluster_definitions = YAML.load(definitions)
+  rescue Etcd::KeyNotFound => e
+    exception = Tendrl::HttpResponseErrorHandler.new(
+      e, cause: '/clusters/definitions', object_id: cluster_id
+    )
+    halt exception.status, exception.body.to_json
+  end
+
+  def cluster(cluster_id)
+    load_definitions(cluster_id)
+    @cluster ||=
+      recurse(etcd.get("/clusters/#{cluster_id}/TendrlContext"))['tendrlcontext']
+  rescue Etcd::KeyNotFound => e
+    exception =  Tendrl::HttpResponseErrorHandler.new(
+      e, cause: '/clusters/id', object_id: cluster_id
+    )
+    halt exception.status, exception.body.to_json
+  end
+
   def authenticate
     if access_token.present?
       @current_user = Tendrl::User.authenticate_access_token(access_token)
