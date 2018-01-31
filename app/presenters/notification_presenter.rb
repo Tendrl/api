3101eb8fad2922ac264b35e27cdba2e9e1828fbf
Rohan Kanade <r0h4n@users.noreply.github.com>
2018-01-31 20:19:27 +0530

    Update notification_presenter.rb

--- app/presenters/notification_presenter.rb
+++ app/presenters/notification_presenter.rb
@@ -24,13 +24,12 @@ module NotificationPresenter
       raw_notifications.each do |notification|
         notification.each do |notification_id, attributes|
           next unless attributes['cluster_id'] == cluster_id
-            attributes.slice!('message_id', 'timestamp', 'priority', 'payload')
-            payload = attributes.delete('payload')
-            message = JSON.parse(payload['message'])['tags']['message'] rescue ""
-            attributes['message_id'] = notification_id
-            attributes['message'] = message
-            notifications << attributes
-          end
+          attributes.slice!('message_id', 'timestamp', 'priority', 'payload')
+          payload = attributes.delete('payload')
+          message = JSON.parse(payload['message'])['tags']['message'] rescue ""
+          attributes['message_id'] = notification_id
+          attributes['message'] = message
+          notifications << attributes
         end
       end
       notifications.sort do |a,b|
