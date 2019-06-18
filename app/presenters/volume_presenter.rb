67d45b85f87bf7a9689d334c96d331aa90358643
GowthamShanmugasundaram <gshanmug@redhat.com>
2019-06-18 05:10:30 +0000

    Stop displaying deleted volumes in tendrl-ui
    bugzilla: 1719033
    tendrl-bug-id: Tendrl/api#463
    
    Signed-off-by: GowthamShanmugasundaram <gshanmug@redhat.com>

--- app/presenters/volume_presenter.rb
+++ app/presenters/volume_presenter.rb
@@ -4,10 +4,12 @@ module VolumePresenter
       volumes = []
       raw_volumes.each do |volume|
         volume.each do |vol_id, attributes|
-          attributes['vol_id'] = vol_id
-          attributes.delete('bricks')
-          attributes.delete('options')
-          volumes << attributes
+          if attributes['deleted'] != true
+            attributes['vol_id'] = vol_id
+            attributes.delete('bricks')
+            attributes.delete('options')
+            volumes << attributes
+          end
         end
       end
       volumes
